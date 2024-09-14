50a6bfaf0a269e189ce0ae6c4993d1cf3e783169
liukaiwen <kl3157@columbia.edu>
2024-09-14 17:59:28 +0800

    feat: test speed improvement

--- demo/demo.py
+++ demo/demo.py
@@ -5,34 +5,45 @@ from loguru import logger
 
 from magic_pdf.pipe.UNIPipe import UNIPipe
 from magic_pdf.rw.DiskReaderWriter import DiskReaderWriter
-
+from magic_pdf.model.crop_image_multiprocess import load_images_from_pdf
 import magic_pdf.model as model_config 
 model_config.__use_inside_model__ = True
 
-try:
-    current_script_dir = os.path.dirname(os.path.abspath(__file__))
-    demo_name = "demo1"
-    pdf_path = os.path.join(current_script_dir, f"{demo_name}.pdf")
-    model_path = os.path.join(current_script_dir, f"{demo_name}.json")
-    pdf_bytes = open(pdf_path, "rb").read()
-    # model_json = json.loads(open(model_path, "r", encoding="utf-8").read())
-    model_json = []  # model_json传空list使用内置模型解析
-    jso_useful_key = {"_pdf_type": "", "model_list": model_json}
-    local_image_dir = os.path.join(current_script_dir, 'images')
-    image_dir = str(os.path.basename(local_image_dir))
-    image_writer = DiskReaderWriter(local_image_dir)
-    pipe = UNIPipe(pdf_bytes, jso_useful_key, image_writer)
-    pipe.pipe_classify()
-    """如果没有传入有效的模型数据，则使用内置model解析"""
-    if len(model_json) == 0:
-        if model_config.__use_inside_model__:
-            pipe.pipe_analyze()
-        else:
-            logger.error("need model list input")
-            exit(1)
-    pipe.pipe_parse()
-    md_content = pipe.pipe_mk_markdown(image_dir, drop_mode="none")
-    with open(f"{demo_name}.md", "w", encoding="utf-8") as f:
-        f.write(md_content)
-except Exception as e:
-    logger.exception(e)
+
+if __name__ == '__main__':
+    import time
+    try:
+        images = None
+        current_script_dir = os.path.dirname(os.path.abspath(__file__))
+        demo_name = "demo1"
+        pdf_path = os.path.join(current_script_dir, f"{demo_name}.pdf")
+        model_path = os.path.join(current_script_dir, f"{demo_name}.json")
+        pdf_bytes = open(pdf_path, "rb").read()
+        start_time = time.time()
+        images = load_images_from_pdf(pdf_bytes, dpi=200, max_worker=8)
+        end_time = time.time()
+        logger.info(f"images crop speed: {len(pdf_bytes)/(end_time-start_time)} pages/s")
+        logger.info(f"images crop cost: {len(pdf_bytes) / (end_time - start_time)} s")
+        # print(f"===========================================image length: {len(images)}")
+        # model_json = json.loads(open(model_path, "r", encoding="utf-8").read())
+        model_json = []  # model_json传空list使用内置模型解析
+        jso_useful_key = {"_pdf_type": "", "model_list": model_json}
+        local_image_dir = os.path.join(current_script_dir, 'images')
+        image_dir = str(os.path.basename(local_image_dir))
+        image_writer = DiskReaderWriter(local_image_dir)
+
+        pipe = UNIPipe(pdf_bytes, jso_useful_key, image_writer, images=images)
+        pipe.pipe_classify()
+        """如果没有传入有效的模型数据，则使用内置model解析"""
+        if len(model_json) == 0:
+            if model_config.__use_inside_model__:
+                pipe.pipe_analyze()
+            else:
+                logger.error("need model list input")
+                exit(1)
+        pipe.pipe_parse()
+        md_content = pipe.pipe_mk_markdown(image_dir, drop_mode="none")
+        with open(f"{demo_name}.md", "w", encoding="utf-8") as f:
+            f.write(md_content)
+    except Exception as e:
+        logger.exception(e)
---
+++ magic_pdf/model/crop_image_multiprocess.py
@@ -0,0 +1,65 @@
+import fitz  # PyMuPDF
+from PIL import Image
+import numpy as np
+from multiprocessing import Pool
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+def process_page(args):
+    """
+    处理单个页面的函数，用于多进程调用
+    参数为一个元组 (page_index, pdf_bytes, dpi)，其中page_index是页面索引，pdf_bytes是PDF的字节流，dpi为分辨率
+    """
+    page_index, pdf_bytes, dpi = args
+
+    # 在子进程中重新打开PDF文档
+    with fitz.open("pdf", pdf_bytes) as doc:
+        page = doc[page_index]
+        mat = fitz.Matrix(dpi / 72, dpi / 72)
+        pm = page.get_pixmap(matrix=mat, alpha=False)
+
+        # 如果宽度或高度在缩放后超过9000，则不再进一步缩放
+        if pm.width > 9000 or pm.height > 9000:
+            pm = page.get_pixmap(matrix=fitz.Matrix(1, 1), alpha=False)
+
+        img = Image.frombytes("RGB", (pm.width, pm.height), pm.samples)
+        img = np.array(img)
+        img_dict = {"image_id": page_index, "img": img, "width": pm.width, "height": pm.height}
+    return img_dict
+
+
+def load_images_from_pdf(pdf_bytes: bytes, dpi=200, max_worker=2) -> list:
+    try:
+        from PIL import Image
+    except ImportError:
+        logger.error("Pillow not installed, please install by pip.")
+        exit(1)
+
+    # 使用上下文管理器打开PDF文档一次，获取总页数
+    with fitz.open("pdf", pdf_bytes) as doc:
+        num_pages = doc.page_count
+
+        # 创建一个固定大小的进程池
+        with Pool(processes=min(max_worker, num_pages)) as pool:
+            # 准备参数列表，每个元素都是一个元组 (page_index, pdf_bytes, dpi)
+            page_args = [(i, pdf_bytes, dpi) for i in range(num_pages)]
+
+            # 使用map应用process_page函数到每个页面上
+            images = pool.map(process_page, page_args)
+    images = sorted(images, key=lambda x:x["image_id"])
+    return images
+
+if __name__ == '__main__':
+    input = "D:\\projects\\Magic-PDF\\demo\\demo1.pdf"
+    with open(input, 'rb') as f:
+        pdf_bytes = f.read()
+
+
+        # 调用函数加载图片
+    images = load_images_from_pdf(pdf_bytes, dpi=200)
+
+    # 输出结果（这里简单输出到控制台）
+    for img in images:
+        print(f"Image size: {img['width']}x{img['height']}, DPI: 200")
